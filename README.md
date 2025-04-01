357a33a983d0cb56a66bbacfa6f45ba64fabcc1d
Zhalslar <zsl2936169201@gmail.com>
2025-04-02 00:38:15 +0800

    初次提交

---
+++ README.md
@@ -0,0 +1,38 @@
+# astrbot_plugin_mcping
+
+## 介绍
+
+基于astrbot的查询MineCraft服务器状态并返回图片的插件
+
+## 📦 安装
+- 安装插件本体
+```bash
+# 克隆仓库到插件目录
+cd /AstrBot/data/plugins
+git clone https://github.com/Zhalslar/astrbot_plugin_mcping
+
+# 控制台重启AstrBot
+```
+- 安装必要依赖：mcstatus，可以直接从astrbot控制台安装（推荐），也可以先进入astrbot的虚拟环境然后运行下面的指令：
+ ```bash
+pip install mcstatus
+```
+
+## ⚙️ 配置
+ 
+请在astrbot面板配置，插件管理 -> astrbot_plugin_mcping -> 操作 -> 插件配置
+
+
+## 🐔 指令
+```bash
+/mcp MC服务器地址  
+```
+
+## 📌 特别感谢
+[nonebot-plugin-mcping](https://github.com/skiesworld/nonebot-plugin-mcping): 基于NoneBot的查询MineCraft服务器状态并返回图片的插件
+
+
+
+## 📜 开源协议
+本项目采用 [GNU AGPLv3](LICENSE)
+
---
+++ main.py
@@ -0,0 +1,30 @@
+
+from astrbot.api.event import filter
+from astrbot.api.star import Context, Star, register
+from astrbot.core.platform import AstrMessageEvent
+from .data_source import get_be_server_status, get_java_server_status
+
+
+@register("astrbot_plugin_mcping", "Zhalslar", "获取 Minecraft JE/BE 服务器 Motd 图片信息", "1.0.0")
+class MCPingPlugin(Star):
+    def __init__(self, context: Context):
+        super().__init__(context)
+        self.temp_image_path = "temp_server_status.png"
+
+
+    @filter.command("mcp",alias={"mcping","MCP"}, desc="获取 Minecraft JE/BE 服务器 Motd 图片信息")
+    async def on_command(self, event: AstrMessageEvent, server_ip: str = None):
+        server_status = await get_java_server_status(server_ip)
+        if not server_status:
+            server_status = await get_be_server_status(server_ip)
+
+        if server_status:
+            with open(self.temp_image_path, "wb") as f:
+                f.write(server_status)
+            yield event.image_result(self.temp_image_path)
+        else:
+            yield event.plain_result("查询失败")
+
+
+
+
